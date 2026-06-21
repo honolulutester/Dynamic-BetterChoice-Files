0d7caa623e6625f0208a7539af2b8a3ab3666e9f
honolulutester <imdauntless201@gmail.com>
2026-06-21 14:52:35 +0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 export const SUPABASE_URL = "https://ihavyjmmecztzugaybqy.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJ...";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloYXZ5am1tZWN6dHp1Z2F5YnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTI4NDIsImV4cCI6MjA5NzU4ODg0Mn0.nYlnRJ51mAyiGnUrMdU-6gjyD9HWQP2tfzx-tZPKh-c";
